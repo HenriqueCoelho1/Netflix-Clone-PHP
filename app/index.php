8385fc434b880020d78c6ba398b439482ca4ea96
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-07-02 18:06:06 -0300

    Categories Class

--- app/index.php
+++ app/index.php
@@ -3,4 +3,7 @@ require_once("includes/header.php");
 
 $preview = new PreviewProvider($con, $user_logged);
 echo $preview->create_preview_video(null);
+
+$containers = new CategoriesContainers($con, $user_logged);
+echo $containers->show_all_categories(null);
 ?>
